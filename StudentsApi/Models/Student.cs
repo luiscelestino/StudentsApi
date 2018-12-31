b11cc5342fd26021107a79097079c1ae69401ee4
Luis Ricardo Celestino de Souza <souza.luis@gmail.com>
2018-12-31 18:10:56 -0300

    Code updated.

---
+++ StudentsApi/Models/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentsApi.Models
+{
+    [Table("alunos")]
+    public class Student
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("nome")]
+        public string Name { get; set; }
+
+        [Required]
+        [Column("cpf")]
+        public string Cpf { get; set; }
+
+        [Required]
+        [Column("email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Column("data_nascimento")]
+        public DateTime BirthDate { get; set; }
+    }
+}
